c21150594c50e6d7728b6bd2ee98cf21f10eeb08
Robertwo <irobertt@outlook.com>
2016-01-11 00:14:56 -0600

    Tarea

--- Contents.swift
+++ Contents.swift
@@ -2,19 +2,24 @@
 
 import UIKit
 
-for a in 0...100 {
+for a in 0...100{
     
-    if a % 10 == 5 || a % 10 == 0 {
-        print("# \(a) Bingo!!!")
+    if(((a % 5) == 0) && a != 0){
+        
+        print("#\(a) Bingo!!!")
     }
-    if a % 10 == 2 || a % 10 == 0{
-        print("# \(a) Par!!!")
+    if(((a % 2) == 0)){
+        
+        print("#\(a) par!!!")
     }
-    if a % 10 == 3 || a % 10 == 1{
-        print("# \(a) Impar!!!")
+    
+    if(((a % 2) == 1)){
+        
+        print("#\(a) impar!!!")
     }
     if a >= 30 && a <= 40 {
-        print("# \(a) Viva Swift!!!")
+        
+        print("#\(a) Viva Swift!!!")
     }
-    
 }
+
